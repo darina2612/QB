a836714b7d9a2c53483ca18ede4be281c729d3e8
Darina Krasteva <darina.krasteva@egt-bg.com>
2020-01-23 16:25:28 +0200

    More performance tests.

--- main.cpp
+++ main.cpp
@@ -8,29 +8,61 @@
 
 void TestPreformance()
 {
-    auto data1 = PerformanceTestHelpers::PopulateDummyData<QBRecord, QBRecordCollection>("testdata", 100000,
-                                                                                        [](auto& collection, const auto& record)
-                                                                                        {
-                                                                                            collection.emplace_back(record);
-                                                                                        });
-    PerformanceTestHelpers::TestPerformance("Base implementation data population and fetch records",
-                                            [&data1]()
+    auto testFetch = [](uint recordsCount)
     {
-        auto filteredSet = QBFindMatchingRecords(data1, "column1", "testdata500");
-        auto filteredSet2 = QBFindMatchingRecords(data1, "column2", "24");
-    });
+        auto data1 = PerformanceTestHelpers::PopulateDummyData<QBRecord, QBRecordCollection>("testdata", recordsCount,
+                                                                                            [](auto& collection, const auto& record)
+                                                                                            {
+                                                                                                collection.emplace_back(record);
+                                                                                            });
+        PerformanceTestHelpers::TestPerformance("Base implementation data population and fetch - "
+                                                + std::to_string(recordsCount) + " records",
+                                                [&data1, &recordsCount]()
+        {
+            auto filteredSet = QBFindMatchingRecords(data1, "column1", "testdata" + std::to_string(recordsCount));
+            auto filteredSet2 = QBFindMatchingRecords(data1, "column2", "24");
+        });
 
-    auto data2 = PerformanceTestHelpers::PopulateDummyData<Record, RecordCollection>("testdata", 100000,
+        auto data2 = PerformanceTestHelpers::PopulateDummyData<Record, RecordCollection>("testdata", recordsCount,
                                                                                         [](auto& collection, const auto& record)
                                                                                         {
                                                                                             collection.Add(record);
                                                                                         });
-    PerformanceTestHelpers::TestPerformance("Data population and fetch records",
-                                            [&data2]()
+        PerformanceTestHelpers::TestPerformance("Data population and fetch - " + std::to_string(recordsCount) + " records",
+                                                [&data2, &recordsCount]()
+        {
+            auto filteredSet = data2.FindMatchingRecords("column1", "testdata" + std::to_string(recordsCount / 2));
+            auto filteredSet2 = data2.FindMatchingRecords("column2", "24");
+        });
+
+        std::cout << "=======================================================\n";
+    };
+
+    auto testRemove = [](uint recordsCount)
     {
-        auto filteredSet = data2.FindMatchingRecords("column1", "testdata500");
-        auto filteredSet2 = data2.FindMatchingRecords("column2", "24");
-    });
+        auto data = PerformanceTestHelpers::PopulateDummyData<Record, RecordCollection>("testdata", recordsCount,
+                                                                                        [](auto& collection, const auto& record)
+                                                                                        {
+                                                                                            collection.Add(record);
+        });
+
+        PerformanceTestHelpers::TestPerformance("Remove with " + std::to_string(recordsCount) + " records",
+                                                [&]()
+        {
+            for(uint i = 0; i < recordsCount / 2; ++i)
+            {
+                data.DeleteRecordByID(i);
+                data.DeleteRecordByID(recordsCount - i - 1);
+            }
+        });
+
+        std::cout << "=======================================================\n";
+    };
+
+    testFetch(10000);
+    testFetch(1000000);
+
+    testRemove(100000);
 }
 
 int main()
