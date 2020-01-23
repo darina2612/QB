2122059a0ec5cb5da2f412fe87c1226a59daf0f4
Darina Krasteva <darina.krasteva@egt-bg.com>
2020-01-23 11:38:08 +0200

    Fixed record deletion.

--- RecordCollection.cpp
+++ RecordCollection.cpp
@@ -29,15 +29,15 @@ void RecordCollection::Add(const Record& record)
 RecordCollection RecordCollection::FindMatchingRecords(const std::string& columnName,
                                                        const std::string& matchString) const
 {
-    RecordCollection resultCollection{};
-
     IdSet matchingIds{};
     if(columnName == column0)
     {
+        // column0's value is unique key, no more thn one record should contain this value
         matchingIds.emplace(std::stoul(matchString));
     }
     else
     {
+        //searching for the set of ids of recors, containig the requested value for that field
         auto it = searchIndex_.find(columnName);
         if(it != searchIndex_.end())
         {
@@ -47,6 +47,9 @@ RecordCollection RecordCollection::FindMatchingRecords(const std::string& column
         }
     }
 
+    RecordCollection resultCollection{};
+
+    //now adding the records to the result collection
     for(const auto& id : matchingIds)
     {
         auto idIt = recordsStorge_.find(id);
@@ -64,7 +67,33 @@ void RecordCollection::DeleteRecordByID(uint id)
     auto it = recordsStorge_.find(id);
 
     if(it != recordsStorge_.end())
+    {
+        //removing record's info from search index
+        auto removeFromIndex = [this, id](const auto& columnName, const auto& valueStr)
+        {
+            auto it = searchIndex_.find(columnName);
+            if(it != searchIndex_.end())
+            {
+                auto valueIt = it->second.find(valueStr);
+                if(valueIt != it->second.end())
+                {
+                    auto& indexSet = valueIt->second;
+
+                    auto indexIt = indexSet.find(id);
+                    if(indexIt != indexSet.end())
+                        indexSet.erase(indexIt);
+                }
+            }
+        };
+
+        const auto& record = it->second;
+        removeFromIndex(column1, record.column1);
+        removeFromIndex(column2, std::to_string(record.column2));
+        removeFromIndex(column3, record.column3);
+
+        //removing the record from storage
         recordsStorge_.erase(it);
+    }
 }
 
 size_t RecordCollection::Size() const
